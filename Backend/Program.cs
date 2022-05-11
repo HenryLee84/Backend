833e1a4a5072b8d3e07844347f426b348ea8df6b
Henry Lee <Henry840220@gmail.com>
2022-05-11 18:22:41 +0800

    加入Repository.proj與Utilities.proj
    Repository 資料存取
    TODO 存取DB
    Utilities 工具
    1. 生成 jwt token

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,10 @@
+using Utilities;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<TokenHelper>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Utilities/TokenHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Utilities
+{
+    public class TokenHelper
+    {
+        private readonly IConfiguration _configuration;
+        public TokenHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// 產生 Jwt Token
+        /// </summary>
+        /// <param name="id">使用者Id</param>
+        /// <param name="account">使用者帳號</param>
+        /// <returns></returns>
+        public string GenerateJwtToken(int id, string account)
+        {
+            var issuer = _configuration.GetValue<string>("Jwt:issuer");
+            var key = _configuration.GetValue<string>("Jwt:key");
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, account),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("id", id.ToString()),
+                new Claim("role", "Admin")
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(issuer, null, claims, DateTime.UtcNow, DateTime.UtcNow.AddMinutes(30), credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
