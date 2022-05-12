1ba7a2ffa9105bc266ded4320da6256dda57834b
Henry Lee <Henry840220@gmail.com>
2022-05-12 11:58:28 +0800

    調整TokenHelper namespace & 新增ClaimPrincipalExtensions用作取資料

---
+++ Utilities/Extensions/ClaimPrincipalExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+
+namespace Utilities.Extensions
+{
+    public static class ClaimPrincipalExtensions
+    {
+        /// <summary>
+        /// 從ClaimsPrincipal取得Id
+        /// </summary>
+        /// <param name="user">ClaimsPrincipal</param>
+        /// <returns></returns>
+        public static int? GetUserID(this ClaimsPrincipal user)
+        {
+            var id = user.FindFirstValue("id");
+
+            if (string.IsNullOrEmpty(id) || !int.TryParse(id, out var result))
+                return null;
+
+            return result;
+        }
+
+        /// <summary>
+        /// 從ClaimsPrincipal取得Account
+        /// </summary>
+        /// <param name="user">ClaimsPrincipal</param>
+        /// <returns></returns>
+        public static string? GetUserAccount(this ClaimsPrincipal user)
+        {
+            var account = user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return account;
+        }
+    }
+}
--- Utilities/Helper/TokenHelper.cs
+++ Utilities/Helper/TokenHelper.cs
@@ -4,7 +4,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace Utilities
+namespace Utilities.Helper
 {
     public class TokenHelper
     {
